57881d4848c62e4c92f14e93344a45661da9cc9c
chienbigstar <chienbigstar@gmail.com>
2016-11-30 22:41:21 +0700

    gbgbbg

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,6 @@ module Fbt4
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.assets.precompile += Ckeditor.assets
   end
 end
