3416911371922803b7101d02649790acd2602cf7
chienbigstar <chienbigstar@gmail.com>
2016-11-30 22:49:14 +0700

    ffff

--- config/application.rb
+++ config/application.rb
@@ -12,5 +12,8 @@ module Fbt4
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.assets.precompile += Ckeditor.assets
+    config.assets.enabled = true
+    config.assets.precompile += Ckeditor.assets
+    config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
   end
 end
